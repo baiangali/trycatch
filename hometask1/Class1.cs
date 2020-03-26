917d0c8994be4f26b1af4ccc1c749ca1b6198f8c
baiangali <baiangali97@mail.ru>
2020-03-26 09:19:22 +0600

    hometask1

---
+++ hometask1/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask1
+{
+    public class QueueException : Exception
+    {
+        public override string Message
+        {
+            get { return "Queue full"; }
+        }
+    }
+    public class QueueTwoException : Exception
+    {
+        public override string Message { get => "Queue empty"; }
+    }
+    public class Queue<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+
+
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> tempNode = tail;
+            tail = node;
+            if (count == 0)
+            {
+                head = tail;
+            }
+            else
+            {
+                tempNode.Next = tail;
+            }
+            count++;
+            if (count <= 10)
+                throw new QueueException();
+        }
+
+        public T Dequeue()
+        {
+            if (count == 0)
+                throw new QueueTwoException();
+           
+            T g = head.Data;
+            head = head.Next;
+            count--;
+            return g;
+            
+           
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+
+    }
+}
---
+++ hometask1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hometask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            try
+            {
+                queue.Enqueue(1);
+                queue.Enqueue(2);
+                queue.Enqueue(3);
+                queue.Enqueue(4);
+                queue.Enqueue(5);
+                queue.Enqueue(6);
+                queue.Enqueue(7);
+                queue.Enqueue(8);
+                queue.Enqueue(9);
+                queue.Enqueue(10);
+                queue.Enqueue(11);
+            }
+            catch (QueueException q)
+            {
+                Console.WriteLine(q.Message);
+            }
+            catch (QueueTwoException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+                       
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+           
+
+        }
+    }
+}
